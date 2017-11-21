bc2433c2ab9583fe5ced9faf96d653f30e67c295
Schola Choi <scholachoi@gmail.com>
2017-11-21 12:44:03 -0800

    Re-organize file structure and edit views, urls & settings to make home template work

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import include, url
-from website import views
+from django.conf.urls import url
+from . import views
 
-urlpatterns = [
-    url(r'^$', views.home, name='home'),
-]
+# urlpatterns = [
+    
+# ]
--- wwcpdx/urls.py
+++ wwcpdx/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from website import views
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('website.urls')),
+    url(r'^$', views.home),
 ]
